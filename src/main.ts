fdd904eb531101f881393477398a5af02f84c956
wasif296 <wasifzahoor296@gmail.com>
2026-01-20 04:15:44 +0500

    deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://digi-media-skill.vercel.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
